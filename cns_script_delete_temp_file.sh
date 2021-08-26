b4ba3882cfc33d8ac99a2b234bf4c8037dfe4e0e
“innovation4j” <innovation4j@outlook.kr>
2021-08-27 00:11:05 +0900

    third

---
+++ cns_script_delete_temp_file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /home/iot/Documents/CNS/cns_script_header.sh
+
+#=============================
+# 8. cns_script_delete_temp_file
+echo "8. [start] cns_script_delete_temp_file"
+#
+echo "9 [done] cns_script_delete_temp_file"
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
---
+++ cns_script_send_log_to_azure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /home/iot/Documents/CNS/cns_script_header.sh
+
+#=============================
+# 7. 각 워크스테이션의 감지로그를 Azure DB로 전송하는 프로그램을 수행한다.
+echo "7. [start] send detected log to Azure SQL"
+#python iot_detect_export.py  # comment : need parameters    MB3 20210811
+echo "7. [done] send detected log to Azure SQL"
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
