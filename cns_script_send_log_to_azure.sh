47fbb520ce80612c50c52e5b9ce087d2acff7ec3
“innovation4j” <innovation4j@outlook.kr>
2021-08-30 14:34:00 +0900

    20210830_03_change_logfilename

--- cns_script_send_log_to_azure.sh
+++ cns_script_send_log_to_azure.sh
@@ -2,11 +2,12 @@
 
 source /home/iot/Documents/CNS/cns_script_header.sh
 
+
 #=============================
 # 7. 각 워크스테이션의 감지로그를 Azure DB로 전송하는 프로그램을 수행한다.
 echo "7. [start] send detected log to Azure SQL"
-python /home/iot/Documents/CNS/iot_detect_export.py $linenumber $today # comment : need parameters    MB3 20210811 # python iot_detect_export.py mb5 20210822
-mv "$homedir"/logs/"$linenumber"/"$today"/EXP/"$today"_"$linenumber"_EXP.log "$homedir"/logs/"$linenumber"/"$today"/EXP/"$today"_"$linenumber"_EXP_$hms.log
-echo mv "$homedir"/logs/"$linenumber"/"$today"/EXP/"$today"_"$linenumber"_EXP.log "$homedir"/logs/"$linenumber"/"$today"/EXP/"$today"_"$linenumber"_EXP_$hms.log
+python /home/iot/Documents/CNS/iot_detect_export.py $linenumber $today # comment : need parameters MB5 20210811
+mv "$homedir"/logs/"$linenumber"/"$today"/EXP/"$today"_"$linenumber"_EXP.log "$homedir"/logs/"$linenumber"/"$today"_$hms/EXP/"$today"_"$linenumber"_EXP.log
+echo mv "$homedir"/logs/"$linenumber"/"$today"/EXP/"$today"_"$linenumber"_EXP.log "$homedir"/logs/"$linenumber"/"$today"_$hms/EXP/"$today"_"$linenumber"_EXP.log
 echo "7. [done] send detected log to Azure SQL"
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
