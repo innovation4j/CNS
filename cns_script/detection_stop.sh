8b2634997e107baa8217c9e0f938c7346a05405e
“innovation4j” <innovation4j@outlook.kr>
2021-09-01 15:27:49 +0900

    modified detection_stop.sh 3

--- cns_script/detection_stop.sh
+++ cns_script/detection_stop.sh
@@ -6,13 +6,14 @@ source /home/iot/Documents/CNS/cns_script/header.sh
 # 1. cns_detection.py 프로세스를 전부 종료
 echo -e "\n$today $hms [start] detection_stop.sh"
 
-#pkill -9 -ef cns_detection.py
-#echo "$today $hms pkill -9 -ef cns_detection.py"
-
 for var in {1..7} ; do
     echo "$today $hms $SUDOPW | sudo -S systemctl stop cns_detection_$lineip$var"
     echo $SUDOPW | sudo -S systemctl stop cns_detection_$lineip$var
 done
+
+pkill -9 -ef cns_detection.py
+echo "$today $hms pkill -9 -ef cns_detection.py"
+
 
 echo -e "$today $hms [done] detection_stop.sh \n"
 sleep 3s
