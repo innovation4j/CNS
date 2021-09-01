aa0de3d0ade15b1b08f885fd47ba14b20dd6d970
“innovation4j” <innovation4j@outlook.kr>
2021-09-01 14:45:22 +0900

    modofy detection_stop.sh

--- cns_script/detection_stop.sh
+++ cns_script/detection_stop.sh
@@ -5,8 +5,13 @@ source /home/iot/Documents/CNS/cns_script/header.sh
 #=============================
 # 1. cns_detection.py 프로세스를 전부 종료
 echo -e "\n$today $hms [start] detection_stop.sh"
-pkill -9 -ef cns_detection.py
-echo "$today $hms pkill -9 -ef cns_detection.py"
+
+#pkill -9 -ef cns_detection.py
+#echo "$today $hms pkill -9 -ef cns_detection.py"
+
+echo "$today $hms $SUDOPW | sudo -S ssystemctl stop cns_detection*"
+echo $SUDOPW | sudo -S systemctl stop cns_detection*
+
 echo -e "$today $hms [done] detection_stop.sh \n"
 sleep 3s
 ps -aux | grep cns
