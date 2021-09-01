374d0b090d7bedcd885ca74e756cc35aaa19cc37
“innovation4j” <innovation4j@outlook.kr>
2021-09-01 16:17:14 +0900

    runshell_modified

--- cns_script/run_1_10_minutes.sh
+++ cns_script/run_1_10_minutes.sh
@@ -4,5 +4,5 @@ source /home/iot/Documents/CNS/cns_script/header.sh
 
 #=============================
 # 1회/Hour xx:00:00 | A1
-$shomedir/send_log_to_azure.sh  &>> $lhomedir/cns_script_log_$sln$app"_10m_"$today.out    # A1: Export Azure - log
-$shomedir/send_img_to_azure.sh  &>> $lhomedir/cns_script_log_$sln$app"_10m_"$today.out    # A1: Export Azure - image
+$shomedir/send_log_to_azure.sh  &>> $lhomedir/log_$today"_"10min.log    # A1: Export Azure - log
+$shomedir/send_img_to_azure.sh  &>> $lhomedir/log_$today"_"10min.log    # A1: Export Azure - image
